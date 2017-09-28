47fcc313b2b9921985b0749484e8b06299257084
kwang7 <kwang7@stuy.edu>
2017-09-27 22:05:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,12 @@ import random_selector
 
 
 #assign following fxn to run when root route requested 
-@app.route("/")
-
-#"Home" page the user sees 
+@app.route("/") 
 def hello_world():
     retStr = ""
-    retStr += "<center>Hello, nice to meet you! Click on the picture for some occupations information!<br>" 
-    retStr += "<a href = /occupations>"
-    retStr += "<img src = /static/quokka.jpg> </a> <br>  </center>"     
+    retStr += "<center>Hello, nice to meet you! I am Mr. Broccupations.<br>" 
+    retStr += "<center><a href = /occupations>Click here to see what I can do!"
+    #retStr += "<img src = /static/quokka.jpg> </a> <br>  </center>"     
     return retStr
 
 
@@ -28,9 +26,7 @@ occupations={}
 
 
 def test_tmplt():
-    #Grabs the dictionary of occupations from random_selector.py 
-    occupations = random_selector.ret_occ()
-    
+    occupations = random_selector.ret_occ() 
     return render_template('occupations_template.html', occupations = occupations, Job = 'Job Class', Percentage = 'Percentage', rand_occ=random_selector.rand_selector())
 
 
